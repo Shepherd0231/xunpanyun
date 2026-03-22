9d8e19fffc152ff8d791cb8e9b6cdd0bfeb041cf
Shepherd0231 <shepherd.shen@gmail.com>
2026-03-22 08:50:32 +0800

    清空仓库

--- src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity';
-import post from './post';
-import product from './product';
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, product],
-};
